6cba7491eb5987ab74e6a6155b4481828b368354
Nguyen Hoang <hoang.nguyen@fruitful.io>
2021-11-14 22:44:27 +0700

    add search and sort

--- src/pages/sanpham/index.tsx
+++ src/pages/sanpham/index.tsx
@@ -1,13 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import Layout from 'Layouts';
 import withAuth from '@hocs/withAuth';
-import { Table, Space, Pagination, DatePicker } from 'antd';
+import { Table, Space, Pagination, DatePicker, Input, Button, Select } from 'antd';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { parse } from 'path/posix';
 import Link from 'next/link';
 import { deleteSanPham, getListSanPham } from '@core/services/API';
 import router from 'next/router';
+
+const { Option } = Select;
+
+const sortSelect = [
+  {
+    id: 1,
+    name: 'name_asc',
+    title: 'Tên A-Z',
+  },
+  {
+    id: 2,
+    name: 'name_desc',
+    title: 'Tên Z-A',
+  },
+  {
+    id: 3,
+    name: 'id_asc',
+    title: 'ID 0-9',
+  },
+  {
+    id: 4,
+    name: 'id_desc',
+    title: 'ID 9-0',
+  },
+];
 
 function index() {
   const [productData, setProductData] = useState<any>();
@@ -20,7 +45,7 @@ function index() {
 
   useEffect(() => {
     getProductList();
-  }, [currentPage]);
+  }, [currentPage, sort]);
 
   const getProductList = async () => {
     getListSanPham(currentPage - 1, pageSize, search, sort)
@@ -90,10 +115,31 @@ function index() {
     setDate(selectedDate);
   }
 
+  const handleSearchChange = ({ target }: any) => {
+    setSearch(target.value);
+  };
+
+  const handleSelectChange = (value: any) => {
+    setSort(value);
+  };
+
   return (
     <Layout title={'Product'}>
       <div>
+        <Input placeholder={'Search'} onChange={handleSearchChange} width="50%" />
+        <Button type="primary" onClick={getProductList}>
+          Search
+        </Button>
         <DatePicker onChange={onChange} />
+        <Select defaultValue={'ID 0-9'} style={{ width: 120 }} onChange={handleSelectChange}>
+          <Option value={'name_asc'}>Tên A-Z</Option>
+          <Option value={'name_desc'}>Tên Z-A</Option>
+          <Option value={'id_asc'}>ID 0-9</Option>
+          <Option value={'id_desc'}>ID 9-0</Option>
+          {/* {sortSelect.map((item, index) => {
+            <Option value={item.name} key={index}>{item.title}</Option>
+          })} */}
+        </Select>
       </div>
       <div>
         <Table columns={columns} dataSource={productData} pagination={false} />
